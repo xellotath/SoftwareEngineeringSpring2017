826ed457dcd3d6c97b9c42e11b277c409ced1044
xellotath <petroswwwww@hotmail.com>
2017-04-09 12:37:30 -0500

    Add files via upload
    Added basis for replays, added delay function, added command synonyms, made few comments around, fixed bug where successfully opening the chest would not show the player the intended message(screen was cleared immediately)

---
+++ Project 201/Latest/Project10/Source.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include "commands.h"
+#include "map.h"
+#include "randomization.h"
+
+
+
+extern FILE *fp;
+extern time_t now;
+extern char date_time[250];
---
+++ Project 201/Latest/Project10/map.c
@@ -0,0 +1,34 @@
+#include "map.h"
+
+//an array of arrays of strings is used to display the map. the uppermost level of the array, and the first of them , holds the original map below.
+//whenever the map is to be randomized, a new one is copied in a different level of the array, indexed by current_level
+//if the randomization is not solvable, then a new instance of the map will be created 
+
+char map[100][25][30] =	
+{
+	"----- ----- ----- ----- -----",
+	"|   | |   | |   | |   | |   |",
+	"|   | |   | |   | |   | |   |",
+	"|   | |   | |   | |   | |   |",
+	"----- ----- ----- ----- -----",
+	"----- ----- ----- ----- -----",
+	"|   | |   | |   | |   | |   |",
+	"|   | |   | |   | |   | |   |",
+	"|   | |   | |   | |   | |   |",
+	"----- ----- ----- ----- -----",
+	"----- ----- ----- ----- -----",
+	"|   | |   | |   | |   | |   |",
+	"|   | |   | |   | |   | |   |",
+	"|   | |   | |   | |   | |   |",
+	"----- ----- ----- ----- -----",
+	"----- ----- ----- ----- -----",
+	"|   | |   | |   | |   | |   |",
+	"|   | |   | |   | |   | |   |",
+	"|   | |   | |   | |   | |   |",
+	"----- ----- ----- ----- -----",
+	"----- ----- ----- ----- -----",
+	"|   | |   | |   | |   | |   |",
+	"|   | |   | |   | |   | |   |",
+	"|   | |   | |   | |   | |   |",
+	"----- ----- ----- ----- -----"
+};

96c9a707866fba4b2cacfd79e948337161dec9f8
Khoi-Ky <msu-kky1@student.mcneese.edu>
2017-04-25 06:21:02 -0500

    High score tracker
    Added high score functionality, still need to write these high scores to a file

--- Source.c
+++ Source.c
@@ -5,12 +5,18 @@
 
 void _delay(unsigned int ms);
 
+void write_replay();
+
+void open_replay();
+
 int algo(int current_try, int(*room_center)[25]);//algo prototype
 
 int a_switch = 0;
 
 int delay_ms = 1000; //delay will be passed to the delay function, and is in miliseconds
 FILE *fp;			 //file pointer
+FILE *replayfile;
+FILE *replayfilenames;
 time_t now;			 //time_t variable for getting current time
 char date_time[250]; //string to hold the date filename for logs
 char date_time_replay[250];//string to hold the date filename for replays
@@ -22,15 +28,25 @@ int back = 0;
 unsigned int ms = 1500;
 int menu = 0; //menu selection variable
 char yn = 0; //yes or no variable
-char latest_replays[3][30] = {{NULL},{NULL},{NULL}}; //array to store replay files
+char latest_replays[3][30] = { { NULL },{ NULL },{ NULL } }; //array to store replay files
+int replay_selection = -1;
+char buffer[30];
+int k, l;
+int highscores[5]; //array to store top 5 high scores
+char names[5][30]; //array to store top 5 player names
+int stored = 0; //variable to determine if a highscore has been stored yet this round, incremented to one when highscore has been stored
+
+
 
 int main(void)
 {
+
+
+
 	now = time(NULL);
 	struct tm *t = localtime(&now);
 	strftime(date_time, sizeof(date_time), "Log_%d-%m-%y_%H-%M-%S.txt", t);
 	fp = fopen(date_time, "w+");
-
 
 
 	int current_try = 1;//this holds the number of tries it has taken for a map to be generated
@@ -43,21 +59,67 @@ int main(void)
 	get_center(room_center);//fill that array
 
 
+
+
+
+
+	open_replay();
+
+
+
+
+
+
+
+
+
+
+	//	}else {
+
+	for (;current_try < 100;current_try++) {
+		//this loop copies the original map that has no pathways
+		//on the next two higher levels of the 3d array to randomize;
+		//saving the original to use as the master map
+		//(there is a note for the need for the extra vesion created at randomization.c)
+
+		for (i = 0;i < 25;i++) {
+			for (j = 0;j < 30;j++) {
+				map[current_try][i][j] = map[0][i][j];
+				map[current_try + 1][i][j] = map[0][i][j];
+			}
+		}
+
+		//it then randomizes the created map
+
+		if (algo(current_try, room_center)) {
+			//it only breaks if the map is valid, which is bound to happen at least
+			//once when the seconds randomization algorithm is used
+			break;
+		}
+
+
+	}
+
 	for (;;)
 	{
-		if (menu == 0) {
+		if (menu == 0) { 
+			system("color 70");
+			open_replay();
+			system("cls");
 			printf("___________\n"
 				"|Main Menu|\n"
 				"-----------\n\n"
 				"1.Play\n"
 				"2.Watch a Replay\n"
-				"3.Exit\n\n"
+				"3.Highscores\n"
+				"4.Exit\n\n"
 				"Your Choice: ");
+			scanf("%d", &menu);
 		}
-		scanf("%d", &menu);
 
-		if (menu == 1) { //activates gameplay
 
+		else if (menu == 1) { //activates gameplay
+			system("color 07");
 			for (;current_try < 100;current_try++) {
 				//this loop copies the original map that has no pathways
 				//on the next two higher levels of the 3d array to randomize;
@@ -82,6 +144,8 @@ int main(void)
 
 			}
 
+			_export(current_try);
+
 			for (;;) {
 				//another endless loop to allow for gameplay
 				//this only ends if the player wins.
@@ -92,6 +156,8 @@ int main(void)
 				for (i = 0;i < 25;i++) {
 					printf("%s\n", map[current_try][i]);
 				}
+
+				clock_t start = clock(), diff; //storing the starting time to be compared to time when finished
 
 				printf("\n");
 
@@ -120,25 +186,59 @@ int main(void)
 					strftime(date_time, sizeof(date_time), "%H-%M-%S - Game event - Player death - "
 						"Player has come in contact with an enemy - Resetting game using the same map", t);
 					fprintf(fp, "%s\n", date_time);
+					_export(current_try);
 				}
 
 				input(current_try);//accepts input from the player and checks it against the defaults
 								   //an output may or may not be explicitly printed
 
 				if (game_ended == 1) {
+					diff = clock() - start; //difference in time from starting time til now
+					//putting time into milli seconds
+					for (int w = 0; w < 5; w++) {
+						if (highscores[w] == NULL && stored == 0) {
+							highscores[w] = diff;
+							printf("Please enter your name: ");
+							scanf("%s", &names[w]);
+							getchar();
+							stored = 1;
+						}
+						else if (highscores[w] != NULL) {
+							if (highscores[w] > diff) {
+								int x = 4 - w;
+								while (x > 0) {
+									highscores[w + x] = highscores[w + x - 1];
+									x--;
+								}
+								highscores[w] = diff;
+								printf("Please enter your name: ");
+								scanf("%s", &names[w]);
+								getchar();
+							}
+						}
+					}
+
 					printf("Thank you for playing!");
 					getchar();
+					game_ended = 0;
 					fclose(fp);
 					system("cls");
+					write_replay();
+					sc = 0;
+					//current_try = 0;
 					menu = 0;
 					break;
 				}
 			}
 		}
-		
+
 		else if (menu == 2) { //activates replay
 
+			stop = 0;
+
 			system("cls");//clear screen
+
+			system("color 70");
 
 			if (latest_replays[0][0] == NULL && latest_replays[1][0] == NULL && latest_replays[2][0] == NULL) {
 				printf("\n"
@@ -154,21 +254,21 @@ int main(void)
 				system("cls");
 				continue;
 			}
-			
+
 			else if (latest_replays[1][0] == NULL && latest_replays[2][0] == NULL) {
 				printf("\n"
 					"___________\n"
 					"| Replays |\n"
 					"-----------\n\n"
 					"Select one of the available replays:\n"
-					"1.\n"
+					"1.%s\n"
 					"2.Return to main menu\n\n"
-					"Your choice: ");
+					"Your choice: ", latest_replays[0]);
 
 				scanf("%d", &menu);
 
 				if (menu == 1) {
-
+					replay_selection = 0;
 				}
 				else if (menu == 2) {
 					system("cls");
@@ -177,7 +277,11 @@ int main(void)
 				}
 				else {
 					printf("Please enter an integer 1 - 2 for menu selection");
-					scanf("%d", &menu);
+					getchar();
+					getchar();
+
+					menu = 2;
+
 				}
 			}
 
@@ -187,15 +291,17 @@ int main(void)
 					"| Replays |\n"
 					"-----------\n\n"
 					"Select one of the available replays:\n"
-					"1.\n"
-					"2.\n"
+					"1.%s\n"
+					"2.%s\n"
 					"3.Return to main menu\n\n"
-					"Your choice: ");
+					"Your choice: ", latest_replays[0], latest_replays[1]);
 				scanf("%d", &menu);
 				if (menu == 1) {
+					replay_selection = 0;
 
 				}
 				else if (menu == 2) {
+					replay_selection = 1;
 
 				}
 				else if (menu == 3) {
@@ -205,29 +311,35 @@ int main(void)
 				}
 				else {
 					printf("Please enter an integer 1 - 3 for menu selection");
-					scanf("%d", &menu);
+					getchar();
+					getchar();
+
+					menu = 2;
 				}
 			}
-			
+
 			else {
 				printf("\n"
 					"___________\n"
 					"| Replays |\n"
 					"-----------\n\n"
 					"Select one of the available replays:\n"
-					"1.\n"
-					"2.\n"
-					"3.\n"
+					"1.%s\n"
+					"2.%s\n"
+					"3.%s\n"
 					"4.Return to main menu\n\n"
-					"Your choice: ");
+					"Your choice: ", latest_replays[0], latest_replays[1], latest_replays[2]);
 				scanf("%d", &menu);
 				if (menu == 1) {
+					replay_selection = 0;
 
 				}
 				else if (menu == 2) {
+					replay_selection = 1;
 
 				}
 				else if (menu == 3) {
+					replay_selection = 2;
 
 				}
 				else if (menu == 4) {
@@ -237,69 +349,135 @@ int main(void)
 				}
 				else {
 					printf("Please enter an integer 1 - 4 for menu selection");
-					scanf("%d", &menu);
+					getchar();
+					getchar();
+
+					menu = 2;
 				}
 			}
 
-			replay_mode = 1;
-			while (steps != sc) {
+			if (replay_selection != -1) {
 
-				/*for (i = 0; i < 35; i++) {
-				_delay(ms);
-				if (_kbhit()) {
-				input(0);
-				}
-				if (i == 34) {
-				stop = 1;
-				}
-				}*/
+				system("color 07");
 
-				if (stop == 1) {
-					break;
-				}
+				replay_mode = 1;
 
 
-				if (pause == 0) {
-					system("cls");
-					for (i = 0;i < 25;i++) {
-						printf("%s\n", replay_map[steps][i]);
+
+				k = 0;
+				l = 0;
+
+				replayfile = fopen(latest_replays[replay_selection], "r");
+
+				while (fgets(buffer, 30, replayfile) != NULL) {
+
+
+					strcpy(replay_map[k][l], buffer);
+					l++;
+					if (l == 25) {
+						l = 0;
+						k++;
 					}
-					_delay(ms);
 
-					if (_kbhit()) {
-						input(0);
-					}
-					steps++;
 				}
-				else {
-					input(0);
-					if (back == 1 || forward == 1) {
+
+				fclose(replayfile);
+
+				while (steps != k) {
+
+
+
+
+
+
+
+					if (pause == 0) {
 						system("cls");
 						for (i = 0;i < 25;i++) {
 							printf("%s\n", replay_map[steps][i]);
 						}
-						forward = 0;
-						back = 0;
+						_delay(ms);
+
+						if (_kbhit()) {
+							input(0);
+							if (stop == 1) {
+								replay_selection = -1;
+								menu = 2;
+								steps = 0;
+								replay_mode = 0;
+								break;
+							}
+						}
+						steps++;
+					}
+					else {
+						input(0);
+						if (back == 1 || forward == 1) {
+							system("cls");
+							for (i = 0;i < 25;i++) {
+								printf("%s\n", replay_map[steps][i]);
+							}
+							forward = 0;
+							back = 0;
+						}
 					}
 				}
+
+
+				if (stop != 1) {
+					for (i = 0; i < 35; i++) {
+						_delay(200);
+						if (_kbhit()) {
+							input(0);
+							if (stop == 1) {
+								i = 34;
+							}
+						}
+						if (i == 34) {
+							menu = 2;
+							replay_selection = -1;
+							replay_mode = 0;
+							steps = 0;
+							break;
+						}
+					}
+				}
+
 			}
-
-			menu = 0;
-			break;
 		}
-
-		else if (menu == 3) { //exits game
+		else if (menu == 3) {
+			system("cls");
+			printf("Highscores: \n\n");
+			int y = 0;
+			if (y < 5 && highscores[y] != NULL) {
+				printf("%d. %s %d seconds and %d milliseconds\n", y+1, names[y], highscores[y] / 1000, highscores[y] % 1000);
+				getchar();
+			}
+			else if (highscores[0] == NULL) {
+				printf("No highscores have been recorded.");
+				getchar();
+			}
+			getchar();
+			menu = 0;
+		}
+		else if (menu == 4) { //exits game
 			system("cls");
 			fclose(fp);
 			return 0;
 		}
 
-		else{
+		else {
 			printf("Please enter an integer 1 - 3 for menu selection");
-			scanf("%d", &menu);
+			menu = 0;
+			getchar();
+			getchar();
 		}
 
 	}
+
+	open_replay();
+
+	getchar();
 
 }
 
@@ -353,4 +531,101 @@ void _delay(unsigned int mseconds) //whenever this function is called it will ca
 {
 	clock_t time = mseconds + clock();	//time to campare against current time
 	while (time > clock());				//when current time is target time, stop
+}
+
+void write_replay() {
+
+	now = time(NULL);
+	struct tm *t = localtime(&now);
+	strftime(date_time, sizeof(date_time), "Replay_%d-%m-%y_%H-%M-%S.txt", t);
+
+	replayfile = fopen(date_time, "w+");
+
+	for (int k = 0; k < sc; k++) {
+		for (int i = 0; i < 25; i++)
+		{
+			fprintf(fp, "%s", replay_map[k][i]);
+		}
+	}
+
+	fclose(replayfile);
+	replayfilenames = fopen("replaylist.txt", "a");
+	fprintf(replayfilenames, "%s\n", date_time);
+	fclose(replayfilenames);
+
+}
+
+
+void open_replay() {
+	int j = 0;
+	int i = 0;
+	char replaylistcontents[100][30];
+
+	replayfilenames = fopen("replaylist.txt", "r");
+
+	if (replayfilenames != NULL) {
+
+		while (fgets(replaylistcontents[i], 30, replayfilenames) != NULL) {
+			i++;
+		}
+		if (i > 3) {
+			fclose(replayfilenames);
+			remove("replaylist.txt");
+			for (j = i - 4;j > -1; j--) {
+				replaylistcontents[j][28] = '\0';
+				remove(replaylistcontents[j]);
+			}
+			replayfilenames = fopen("replaylist.txt", "w");
+			for (j = 0;j <= 2; j++) {
+				replaylistcontents[i - 3][28] = '\0';
+				fprintf(replayfilenames, "%s\n", replaylistcontents[i - 3]);
+				i++;
+			}
+			fclose(replayfilenames);
+			replayfilenames = fopen("replaylist.txt", "r");
+			i = 0;
+			while (fgets(replaylistcontents[i], 30, replayfilenames) != NULL) {
+				i++;
+			}
+			for (j = 0; j < 3; j++) {
+				strcpy(latest_replays[j], replaylistcontents[i - 1]);
+				i--;
+				latest_replays[j][28] = '\0';
+			}
+		}
+		else if (i == 2) {
+			for (j = 0; j < 2; j++) {
+				strcpy(latest_replays[j], replaylistcontents[i - 1]);
+				i--;
+				latest_replays[j][28] = '\0';
+			}
+		}
+		else if (i == 1) {
+			for (j = 0; j < 1; j++) {
+				strcpy(latest_replays[j], replaylistcontents[i - 1]);
+				i--;
+				latest_replays[j][28] = '\0';
+			}
+		}
+		else {
+			for (j = 0; j < 3; j++) {
+				strcpy(latest_replays[j], replaylistcontents[i - 1]);
+				i--;
+				latest_replays[j][28] = '\0';
+			}
+		}
+
+
+
+
+
+
+
+
+		fclose(replayfilenames);
+	}
+
+
+	return 0;
+
 }
